be0f9cc1f6b376cb7d6a8a80f3f6cf1dcb9a0d3e
Vardan Manucharyan <sd003gm@gmail.com>
2016-05-27 20:09:00 +0300

    cleanup

--- frontend/src/Index.js
+++ frontend/src/Index.js
@@ -4,8 +4,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route, hashHistory } from 'react-router';
 import App from './components/App';
-import PoweredBy from './components/Powered-by';
-import About from './components/About';
 import Trace from './components/Trace';
 
 window.React = React;
@@ -13,8 +11,6 @@ window.React = React;
 render(
   (<Router history={hashHistory}>
     <Route path="/" component={App}>
-      <Route path="/about" component={About} />
-      <Route path="/poweredby" component={PoweredBy} />
       <Route path="/trace" component={Trace} />
     </Route>
   </Router>), document.getElementById('content')
